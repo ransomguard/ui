6d3ac01d241de23ad680a29ed425d5e4d040ff21
kimzuni <me@kimzuni.com>
2026-09-20 16:05:26 +0900

    style(ui): update 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,28 @@
+import { Link } from "react-router";
+import { ArrowLeft } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+
+
 export default function NotFound() {
 	return (
-		<div>
-			404 Not Found
+		<div className="flex flex-col gap-4 items-center justify-center text-center pt-64">
+			<span className="text-7xl font-extrabold tracking-widest text-muted-foreground/40">
+				404
+			</span>
+			<h2 className="text-3xl font-bold tracking-tight">
+				Not Found
+			</h2>
+			<div className="pt-4">
+				<Button
+					nativeButton={false}
+					render={<Link to="/"/>}
+				>
+					<ArrowLeft/>
+					Go Home
+				</Button>
+			</div>
 		</div>
 	);
 }
