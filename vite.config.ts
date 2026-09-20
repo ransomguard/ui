27b9db0f2fbf09f438debc3103b0c02d851506fb
kimzuni <me@kimzuni.com>
2026-09-20 14:48:26 +0900

    chore: bump and react-compile

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import path from "node:path";
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
 import electron from "vite-plugin-electron/simple";
 
 
@@ -9,6 +10,11 @@ import electron from "vite-plugin-electron/simple";
 export default defineConfig({
 	plugins: [
 		react(),
+		babel({
+			presets: [
+				reactCompilerPreset(),
+			],
+		}),
 		electron({
 			main: {
 				// Shortcut of `build.lib.entry`.
