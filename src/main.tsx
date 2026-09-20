571789c7a8beedb5c8ecd0e1a2f2a480d08053b7
kimzuni <me@kimzuni.com>
2026-09-20 13:45:18 +0900

    chore init vite with electron

---
+++ src/main.tsx
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import "./index.css";
+import App from "./App.tsx";
+
+
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App/>
+	</StrictMode>,
+);
+
+
+
+// Use contextBridge, for electron
+window.ipcRenderer?.on("main-process-message", (_event, message) => {
+	console.log(message);
+});
